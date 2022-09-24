50a7f6d10906d81b032281fa670e952a8d81168d
ruslan <rus.pro94@bk.ru>
2022-09-24 18:08:36 +0500

    .\shablon000000\

---
+++ shablon000000/Program.cs
@@ -0,0 +1,42 @@
+﻿// Найти третье число
+
+int ReadData(string line)
+{
+Console.WriteLine(line);
+int number = int.Parse(Console.ReadLine() ?? "");
+return number;
+}
+
+char ThirdNumChar(string inputNumber)
+{
+char[] simbols = inputNumber.ToCharArray();
+return simbols[2];
+}
+
+int ThirdNumInt(int number)
+{
+int lenNum = (int)Math.Log10(number);
+int outNumber = (int)(number/Math.Pow(10,lenNum-2))%10;
+return outNumber;
+}
+
+void PrintData(string prefix, string value)
+{
+Console.WriteLine(prefix + value);
+}
+
+
+int number = ReadData("Введите число: ");
+if (number < 100)
+{
+PrintData("Такой цифры нет", "");
+}
+else
+{
+char thirdSimbol = ThirdNumChar(number.ToString());
+PrintData("Введенное вами число: ", thirdSimbol.ToString());
+
+int thirdSimbolInt = ThirdNumInt(number);
+PrintData("Введенное вами число: ", thirdSimbolInt.ToString());
+}
+
