3895db1814cd34ab42e3abc1d04e7b1aa3aee6b9
ruslan <rus.pro94@bk.ru>
2022-09-27 12:22:47 +0500

    .\022kvadratchisel\

---
+++ 022kvadratchisel/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Напишите программу,
+ которая принимает на вход число (N) 
+ и выдаёт таблицу квадратов чисел от 1 до N.
+*/
+
+int ReadData(string line)
+{
+Console.WriteLine(line);
+int number = int.Parse(Console.ReadLine() ?? "");
+return number;
+}
+
+void PrintData(string prefix, string value)
+{
+Console.WriteLine(prefix + value);
+}
+
+
+string LineNum(int numN,int p)
+{
+    string outLine = " 1 ";
+    for (int i = 2; i <= numN; i++)
+    {
+        outLine = outLine + ", " + Math.Pow(i , p);
+    
+    }
+    return outLine;
+}
+
+int numN = ReadData("Введите число N: ");
+
+string lineTop = LineNum(numN,1);
+string lineDown = LineNum(numN,2);
+
+PrintData("",lineTop);
+PrintData("",lineDown);
+
